16697930d0ef7a7a989ebf9c5c6b622011d4a2ed
KabeerSohail <sohail.mohammad@wenable.com>
2022-12-05 13:01:40 +0530

    some changes

--- app/src/main/java/com/example/hybridcryptographywithfirebase/models/ViewPagerItem.kt
+++ app/src/main/java/com/example/hybridcryptographywithfirebase/models/ViewPagerItem.kt
@@ -2,7 +2,9 @@ package com.example.hybridcryptographywithfirebase.models
 
 //data class ViewPagerItem(val broadcastMessage: SingleItem = SingleItem("one", Status.NOT_ACKNOWLEDGED.ordinal))
 
-data class SingleItem(val htmlPage: String = "0", var status: Int = Status.NOT_ACKNOWLEDGED.ordinal)
+const val HTML_ONE = "<!DOCTYPE html> <html> <head> <title>Page Title</title> </head> <body>  <h1>This is a Heading</h1> <p>This is a paragraph.</p>  </body> </html>"
+
+data class SingleItem(val htmlPage: String = HTML_ONE, var status: Int = Status.NOT_ACKNOWLEDGED.ordinal)
 
 enum class Status {
     NOT_ACKNOWLEDGED,
